7ee0fe4236f400513e1a94be5fe291c06baa41ba
wangly <wangly@visionet.com.cn>
2015-07-30 11:31:49 +0800

    2.power of two

---
+++ Power-of-Two.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  power of two
+//
+//  Created by 路遥 王 on 15/7/29.
+//  Copyright (c) 2015年 verity wang. All rights reserved.
+
+// problem:
+// Given an integer, write a function to determine if it is a power of two.
+
+
+#include <iostream>
+
+bool isPowerOfTwo(int n);
+
+bool isPowerOfTwo2(int n);
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "the answer is"<<isPowerOfTwo(16);
+    return 0;
+}
+
+bool isPowerOfTwo(int n) {
+    if(n<=0){
+        return false;
+    }
+    
+    int m = 0;
+    while(n>1){
+        m= n%2;
+        n=n/n;
+        if(m!=0){
+            return false;
+        }
+    }
+    return true;
+}
+bool isPowerOfTwo2(int n){
+    return n>0 && !(n&(n-1));
+}
+
+//sum up: 错了三次，1.理解错题目 2.m取余符号用错了 3.没有考虑n<=0
